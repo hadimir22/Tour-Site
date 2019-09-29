e2c88dd7c41bae0616e9ad96ec31ab42415b70a6
hadimir22 <hadimir22@gmail.com>
2019-09-29 13:17:39 +0530

    heading created with animation

--- src/screens/index.js
+++ src/screens/index.js
@@ -20,6 +20,34 @@ export default class Main extends Component {
             </a>
           </div>
         </header>
+
+        {/* <section className="grid-test">
+          <div className="row">
+            <div className="col-1-of-2">col </div>
+            <div className="col-1-of-2">col </div>
+          </div>
+
+          <div className="row">
+            <div className="col-1-of-3">col </div>
+            <div className="col-1-of-3"> col</div>
+            <div className="col-1-of-3"> col</div>
+          </div>
+        </section> */}
+
+        <main>
+          <section className="section-about">
+            <div className="u-center-text u-margin-bottom-80">
+              <h2 className="heading-secondary">
+                Exciting tour for adventures people
+              </h2>
+            </div>
+
+            <div className="row">
+              <div className="col-1-of-2">text content </div>
+              <div className="col-1-of-2">image compos </div>
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
