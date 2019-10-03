e245d11ece96e7db1d94043bbce461b9009aa3a2
hadimir22 <hadimir22@gmail.com>
2019-10-03 08:01:30 +0530

    text reshaped

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,6 +3,7 @@ import Logo from "../assets/img/logo-white.png";
 import "../style/index.scss";
 import About from "./about";
 import Features from "./features";
+import Packages from "./packages";
 
 export default class Main extends Component {
   render() {
@@ -26,118 +27,37 @@ export default class Main extends Component {
         <main>
           <About />
           <Features />
-
-          <section className="section-tours">
+          <Packages />
+          <section className="section-stories">
             <div className="u-center-text u-margin-bottom-big">
-              <h2 className="heading-secondary">Most popular tours</h2>
+              <h2 className="heading-secondary">We make people happy</h2>
             </div>
 
             <div className="row">
-              <div className="col-1-of-3">
-                <div className="card">
-                  <div className="card__side card__side--front ">
-                    <div className="card__picture card__picture--1">&nbsp;</div>
-                    <h4 className="card__heading">
-                      <span className="card__heading-span card__heading-span--1">
-                        The sea explorer
-                      </span>
-                    </h4>
-                    <div className="card__details">
-                      <ul>
-                        <li>3 day tour</li>
-                        <li>upto 30 people</li>
-                        <li>cozy hotel</li>
-                        <li>2 tour guides</li>
-                        <li>difficulty: easy</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="card__side card__side--back card__side--back-1 ">
-                    <div className="card__cta">
-                      <div className="card__price-box ">
-                        <p className="card__price-only">only</p>
-                        <p className="card__price-value">$599</p>
-                      </div>
-                      <a href="#" className="btn btn--white">
-                        {" "}
-                        Book Now
-                      </a>
-                    </div>
-                  </div>
+              <div className="story">
+                <figure className="story__shape">
+                  <img
+                    src={require("../assets/img/nat-8.jpg")}
+                    alt="person"
+                    className="story__img"
+                  />
+                </figure>
+                <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">
+                    I had the best week in
+                  </h3>
+                  <p>
+                    {" "}
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy. Lorem Ipsum is simply dummy text
+                    of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy
+                  </p>
                 </div>
               </div>
-              <div className="col-1-of-3">
-                <div className="card">
-                  <div className="card__side card__side--front ">
-                    <div className="card__picture card__picture--2">&nbsp;</div>
-                    <h4 className="card__heading">
-                      <span className="card__heading-span card__heading-span--2">
-                        The forest tiger
-                      </span>
-                    </h4>
-                    <div className="card__details">
-                      <ul>
-                        <li>3 day tour</li>
-                        <li>upto 30 people</li>
-                        <li>cozy hotel</li>
-                        <li>separate tents</li>
-                        <li>difficulty: medium</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="card__side card__side--back card__side--back-2 ">
-                    <div className="card__cta">
-                      <div className="card__price-box ">
-                        <p className="card__price-only">only</p>
-                        <p className="card__price-value">$699</p>
-                      </div>
-                      <a href="#" className="btn btn--white">
-                        {" "}
-                        Book Now
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-1-of-3">
-                <div className="card">
-                  <div className="card__side card__side--front ">
-                    <div className="card__picture card__picture--3">&nbsp;</div>
-                    <h4 className="card__heading">
-                      <span className="card__heading-span card__heading-span--3">
-                        The snow adventurer
-                      </span>
-                    </h4>
-                    <div className="card__details">
-                      <ul>
-                        <li>5 day tour</li>
-                        <li>upto 15 people</li>
-                        <li>cozy hotel</li>
-                        <li>2 tour guides</li>
-                        <li>difficulty: hard</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="card__side card__side--back card__side--back-3 ">
-                    <div className="card__cta">
-                      <div className="card__price-box ">
-                        <p className="card__price-only">only</p>
-                        <p className="card__price-value">$899</p>
-                      </div>
-                      <a href="#" className="btn btn--white">
-                        {" "}
-                        Book Now
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="u-center-text u-margin-top-huge">
-              <a href="#" className="btn btn--green">
-                discover all tours
-              </a>
             </div>
           </section>
         </main>
---
+++ src/screens/packages.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+
+export default class Packages extends Component {
+  render() {
+    return (
+      <section className="section-tours">
+        <div className="u-center-text u-margin-bottom-big">
+          <h2 className="heading-secondary">Most popular tours</h2>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-3">
+            <div className="card">
+              <div className="card__side card__side--front ">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <h4 className="card__heading">
+                  <span className="card__heading-span card__heading-span--1">
+                    The sea explorer
+                  </span>
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>3 day tour</li>
+                    <li>upto 30 people</li>
+                    <li>cozy hotel</li>
+                    <li>2 tour guides</li>
+                    <li>difficulty: easy</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card__side card__side--back card__side--back-1 ">
+                <div className="card__cta">
+                  <div className="card__price-box ">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$599</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    {" "}
+                    Book Now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-1-of-3">
+            <div className="card">
+              <div className="card__side card__side--front ">
+                <div className="card__picture card__picture--2">&nbsp;</div>
+                <h4 className="card__heading">
+                  <span className="card__heading-span card__heading-span--2">
+                    The forest tiger
+                  </span>
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>3 day tour</li>
+                    <li>upto 30 people</li>
+                    <li>cozy hotel</li>
+                    <li>separate tents</li>
+                    <li>difficulty: medium</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card__side card__side--back card__side--back-2 ">
+                <div className="card__cta">
+                  <div className="card__price-box ">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$699</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    {" "}
+                    Book Now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-1-of-3">
+            <div className="card">
+              <div className="card__side card__side--front ">
+                <div className="card__picture card__picture--3">&nbsp;</div>
+                <h4 className="card__heading">
+                  <span className="card__heading-span card__heading-span--3">
+                    The snow adventurer
+                  </span>
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>5 day tour</li>
+                    <li>upto 15 people</li>
+                    <li>cozy hotel</li>
+                    <li>2 tour guides</li>
+                    <li>difficulty: hard</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card__side card__side--back card__side--back-3 ">
+                <div className="card__cta">
+                  <div className="card__price-box ">
+                    <p className="card__price-only">only</p>
+                    <p className="card__price-value">$899</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    {" "}
+                    Book Now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="u-center-text u-margin-top-huge">
+          <a href="#" className="btn btn--green">
+            discover all tours
+          </a>
+        </div>
+      </section>
+    );
+  }
+}
