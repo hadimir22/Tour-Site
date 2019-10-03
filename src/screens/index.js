4bd99570046da7fae0c75bd17b9bf0d7f6fcfd14
hadimir22 <hadimir22@gmail.com>
2019-10-03 09:41:59 +0530

    stoies completed

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,6 +4,8 @@ import "../style/index.scss";
 import About from "./about";
 import Features from "./features";
 import Packages from "./packages";
+import VideoMP4 from "../assets/img/video.mp4";
+import VideoWebm from "../assets/img/video.webm";
 
 export default class Main extends Component {
   render() {
@@ -28,7 +30,15 @@ export default class Main extends Component {
           <About />
           <Features />
           <Packages />
+
           <section className="section-stories">
+            <div className="bg-video">
+              <video className="bg-video__content" autoPlay loop muted>
+                <source src={VideoMP4} type="video/mp4" />
+                <source src={VideoWebm} type="video/webm" />
+                Browser not suporting videos
+              </video>
+            </div>
             <div className="u-center-text u-margin-bottom-big">
               <h2 className="heading-secondary">We make people happy</h2>
             </div>
@@ -41,13 +51,13 @@ export default class Main extends Component {
                     alt="person"
                     className="story__img"
                   />
+                  <figcaption className="story__caption">Mary Smith</figcaption>
                 </figure>
                 <div className="story__text">
                   <h3 className="heading-tertiary u-margin-bottom-small">
                     I had the best week in
                   </h3>
                   <p>
-                    {" "}
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy. Lorem Ipsum is simply dummy text of the
@@ -58,6 +68,39 @@ export default class Main extends Component {
                   </p>
                 </div>
               </div>
+            </div>
+
+            <div className="row">
+              <div className="story">
+                <figure className="story__shape">
+                  <img
+                    src={require("../assets/img/nat-9.jpg")}
+                    alt="person"
+                    className="story__img"
+                  />
+                  <figcaption className="story__caption">will smith</figcaption>
+                </figure>
+                <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">
+                    Life changing trip
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy. Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy. Lorem Ipsum is simply dummy text
+                    of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="u-center-text u-margin-top-huge">
+              <a href="#" className="btn-text">
+                Read all Stories &rarr;
+              </a>
             </div>
           </section>
         </main>
